bd2ecf2af2acb62ba891c7d0bc632d2b933a503d
Harper Frankstone <Harper Frankstone>
2017-01-24 15:26:24 -0600

    work on the spanish module

--- javascripts/spanish.js
+++ javascripts/spanish.js
@@ -1,2 +1,27 @@
 "use strict";
 
+var dictionary = (function('variable in main.js'){
+	//the available words that can be translated
+	var spanish = {
+		"merry": "alegre",
+		"christmas": "Navidad",
+		"and": "y",
+		"happy": "contendo",
+		"new": "nuevo",
+		"year": "año"
+	};
+	//grabbing the english 'key' from the above object
+	'variable in main.js'.getEnglish = function(word){
+		return spanish[word];
+	};
+	//using the English word to see what it's translation is 
+	'variable in main.js'.setSpanish = function(newWord){
+		console.log("word choice: ", newWord)
+		spanish[newWord] = newWord.value;
+		console.log("translated word: ", spanish)
+	};
+
+	return 'variable in main.js';
+
+
+})('variable in main.js');
